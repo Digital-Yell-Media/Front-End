ff3243a38d6e3294faafdb2b13a3c3e6f3c80343
droneal11015 <droneal11015@gmail.com>
2019-11-12 21:05:28 -0500

    updated logo component, started nav component, deleted unneccessary files

--- dym/src/App.js
+++ dym/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LogoComponent from './components/logo';
+//import Nav from './components/navBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <LogoComponent/>
         <p>
-         Digital Yell Media, Coming Soon!
+         Coming Soon!
         </p>
         
       </header>
