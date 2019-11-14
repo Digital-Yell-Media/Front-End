8c5c6bd1d2e3b549c6e9bb3be21fb2e7597ad486
droneal11015 <droneal11015@gmail.com>
2019-11-13 22:23:51 -0500

    got routes working and nav rendering

--- dym/src/App.jsx
+++ dym/src/App.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import './App.css';
-import LogoComponent from './components/logo';
-import NavBar from './components/navBar'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Contact from './components/contactForm';
+import Home from './components/home';
+import Services from './components/services';
+import Work from './components/work';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar/>
-        <p>
-          <LogoComponent/>
-         Coming Soon!
-        </p>
-        
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to={'/'} className="nav-link">Home</Link></li>
+              <li><Link to={'/services'} className="nav-link">Services</Link></li>
+              <li><Link to={'/work'} className="nav-link">Work</Link></li>
+              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            </ul>
+          </nav>
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/services" component={Services}/>
+            <Route path="/work" component={Work}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- dym/src/index.jsx
+++ dym/src/index.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Contact from './components/contactForm';
-import Home from './components/home';
-import Services from './components/services';
-import Work from './components/work';
+import App from './App';
 
-import { Router, Route, hashHistory } from 'react-router';
-
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Home}/>
-    <Route path="/services" component={Services}/>
-    <Route path="/work" component={Work}/>
-    <Route path="/contact" component={Contact}/>
-  </Router>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('App'));
